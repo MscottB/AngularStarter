3e8207c2f44a0eefafdea01c04e66675ec05e88b
MscottB <skot_varg@hotmail.com>
2018-05-10 14:33:00 -0300

    update module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//components
+import { AppComponent } from "./app.component";
+
+const routes: Routes = [
+  { path: 'app', component: AppComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'app' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
